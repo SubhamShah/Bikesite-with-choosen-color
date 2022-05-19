d6c24ee5c052d230e6382fea438a7c1dd35f43ce
Subham Shah <sshah0626@gmail.com>
2022-05-19 10:08:46 +0545

    Bike site choose color to see changed image with particular color choosen

---
+++ bike.js
@@ -0,0 +1,14 @@
+let redBike= document.getElementById("red");
+let blueBike= document.getElementById("blue");
+let blackBike= document.getElementById("black");
+let bike=document.getElementById("bike");
+
+redBike.onclick=function(){
+   bike.style.backgroundImage="url(BMW1.png)";
+}
+blueBike.onclick=function(){
+    bike.style.backgroundImage="url(BMW2.png)";
+ }
+ blackBike.onclick=function(){
+    bike.style.backgroundImage="url(BMW3.png)";
+ }
